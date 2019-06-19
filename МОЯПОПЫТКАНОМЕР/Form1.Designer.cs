3b76719eb994b4c75b6e75a239379a802cd844fb
Mike Penchev <48203051+Vm2604@users.noreply.github.com>
2019-06-19 17:26:50 +0300

    Add files via upload

---
+++ МОЯПОПЫТКАНОМЕР/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace lab2sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Calculate = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.Rectangle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textHeight = new System.Windows.Forms.TextBox();
+            this.textHeigth_par = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(538, 197);
+            this.Calculate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(108, 46);
+            this.Calculate.TabIndex = 0;
+            this.Calculate.Text = "Start";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(258, 155);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(204, 154);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(11, 155);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(204, 154);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // Rectangle
+            // 
+            this.Rectangle.AutoSize = true;
+            this.Rectangle.Location = new System.Drawing.Point(11, 137);
+            this.Rectangle.Name = "Rectangle";
+            this.Rectangle.Size = new System.Drawing.Size(72, 17);
+            this.Rectangle.TabIndex = 3;
+            this.Rectangle.Text = "Rectangle";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(254, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Prisma";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textHeight
+            // 
+            this.textHeight.Location = new System.Drawing.Point(14, 48);
+            this.textHeight.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textHeight.Name = "textHeight";
+            this.textHeight.Size = new System.Drawing.Size(89, 22);
+            this.textHeight.TabIndex = 5;
+            // 
+            // textHeigth_par
+            // 
+            this.textHeigth_par.Location = new System.Drawing.Point(258, 48);
+            this.textHeigth_par.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textHeigth_par.Name = "textHeigth_par";
+            this.textHeigth_par.Size = new System.Drawing.Size(89, 22);
+            this.textHeigth_par.TabIndex = 7;
+            this.textHeigth_par.TextChanged += new System.EventHandler(this.textHeigth_par_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Width";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(258, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Height of Prisma";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Save ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(425, 341);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(8, 8);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(261, 314);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Save ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(95, 314);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Open";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(342, 314);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Open";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 360);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textHeigth_par);
+            this.Controls.Add(this.textHeight);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Rectangle);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Calculate);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label Rectangle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textHeight;
+        private System.Windows.Forms.TextBox textHeigth_par;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ МОЯПОПЫТКАНОМЕР/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
+namespace lab2sharp
+{
+    public partial class Form1 : Form
+    {
+        Rectangle rectangle;
+        Prisma parallelepiped;
+
+        public Form1()
+        {
+            InitializeComponent();
+            rectangle = new Rectangle();
+            parallelepiped = new Prisma();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rectangle.width = Convert.ToDouble(textHeight.Text == " " ? "0" : textHeight.Text);
+            parallelepiped.WidthBase = rectangle.width;
+            parallelepiped.WidthBase = rectangle.Width;
+            richTextBox2.Text = rectangle.ToString();
+
+            parallelepiped.Height_of_Prism = Convert.ToDouble(textHeigth_par.Text == " " ? "0" : textHeigth_par.Text);
+            richTextBox1.Text = parallelepiped.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textHeigth_par_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (Stream s = File.Open(saveFileDialog1.FileName, FileMode.CreateNew)) 
+                using (StreamWriter sw = new StreamWriter(s)) 
+                {
+                    sw.Write(richTextBox2.Text);
+                }
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (Stream s = File.Open(saveFileDialog1.FileName, FileMode.CreateNew))
+                using (StreamWriter sw = new StreamWriter(s))
+                {
+                    sw.Write(richTextBox1.Text);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Stream  myStream;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if(openFileDialog1.ShowDialog()== System.Windows.Forms.DialogResult.OK)
+            {
+                if ((myStream=openFileDialog1.OpenFile())!=null)
+                {
+                    string strfilename = openFileDialog1.FileName;
+                    string filetext = File.ReadAllText(strfilename);
+                    richTextBox2.Text = filetext;
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            Stream myStream;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if ((myStream = openFileDialog1.OpenFile()) != null)
+                {
+                    string strfilename = openFileDialog1.FileName;
+                    string filetext = File.ReadAllText(strfilename);
+                    richTextBox1.Text = filetext;
+                }
+            }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
